1af86c3a0955714e52ab9a4d516552841b66e937
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-07-18 22:50:25 +0530

    Initial commit

---
+++ app/src/main/java/com/sama/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvWeatherData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        tvWeatherData = findViewById(R.id.tv_weather_data);
+
+        String[] weatherData = {
+                "Mon -> 19° Cloudy",
+                "Tues -> 29° Clear",
+                "Wed -> 39° Clear",
+                "Thurs -> 19° Thunderstorm",
+                "Fri -> 69°  Clear",
+                "Sat -> 59°  Clear",
+                "Sun -> 99°  Clear",
+                "Mon -> 109° Clear",
+                "Tue -> 39° Clear",
+                "Wed -> 49° Clear",
+        };
+        for (String data : weatherData) {
+            tvWeatherData.append(data + "\n\n\n");
+        }
+    }
+}
