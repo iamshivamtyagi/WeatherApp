687421eea2986fd7d18456181fbc553a60cf4b55
Shivam Tyagi <32505069+iamshivamtyagi@users.noreply.github.com>
2019-08-13 23:31:04 +0530

    utils & networking updated

--- app/src/main/java/com/sama/MainActivity.java
+++ app/src/main/java/com/sama/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sama;
 
 import android.os.AsyncTask;
@@ -6,42 +21,52 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sama.data.DefaultPreferences;
 import com.sama.utilities.NetworkUtils;
-
+import com.sama.utilities.OpenWeatherJsonUtils;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvWeatherData;
-    
+    private TextView mWeatherTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_forecast);
 
-        tvWeatherData = findViewById(R.id.tv_weather_data);
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
 
+        loadWeatherData();
     }
 
-    public class NetworkBackgroundTask extends AsyncTask<String, Void, String[]> {
+    private void loadWeatherData() {
+        String location = DefaultPreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
         @Override
         protected String[] doInBackground(String... params) {
 
             if (params.length == 0) {
                 return null;
             }
-            String location = params[0];
 
+            String location = params[0];
             URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
             try {
-                String jsonWeatherResponse =
-                        NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
-                String[] simpleJsonWeatherData = JsonUtils
-                        .getSimpleWeatherStringsFromJson(MainActivity.this,jsonWeatherResponse);
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
                 return simpleJsonWeatherData;
-            }catch (Exception e){
+
+            } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
@@ -49,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
-            if(weatherData!=null){
-                for(String weatherString : weatherData){
-                    tvWeatherData.append(weatherString + "\n\n\n");
+            if (weatherData != null) {
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
             }
         }
--- app/src/main/java/com/sama/utilities/NetworkUtils.java
+++ app/src/main/java/com/sama/utilities/NetworkUtils.java
@@ -12,53 +12,65 @@ import java.util.Scanner;
 
 public final class NetworkUtils {
 
-    public static final String TAG = NetworkUtils.class.getSimpleName();
+    private static final String TAG = NetworkUtils.class.getSimpleName();
 
-    public static final String STATIC_WEATHER_URL =
+    private static final String DYNAMIC_WEATHER_URL =
+            "https://andfun-weather.udacity.com/weather";
+
+    private static final String STATIC_WEATHER_URL =
             "https://andfun-weather.udacity.com/staticweather";
 
-    public static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
 
-    public static final String format = "json";
+    private static final String format = "json";
 
-    public static final String units = "metric";
+    private static final String units = "metric";
 
-    public static final int numDays = 14;
+    private static final int numDays = 14;
 
     final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
     final static String FORMAT_PARAM = "mode";
     final static String UNITS_PARAM = "units";
     final static String DAYS_PARAM = "cnt";
 
-    public static URL buildUrl(String locationQuery){
+    public static URL buildUrl(String locationQuery) {
         Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                .appendQueryParameter(QUERY_PARAM,locationQuery)
-                .appendQueryParameter(FORMAT_PARAM,format)
-                .appendQueryParameter(UNITS_PARAM,units)
-                .appendQueryParameter(DAYS_PARAM,Integer.toString(numDays))
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                 .build();
+
         URL url = null;
-        try{
+        try {
             url = new URL(builtUri.toString());
-        }catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        Log.v(TAG,"Built URI " + url);
+
+        Log.v(TAG, "Built URI " + url);
+
         return url;
+    }
+
+    public static URL buildUrl(Double lat, Double lon) {
+        return null;
     }
 
     public static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try{
+        try {
             InputStream in = urlConnection.getInputStream();
 
             Scanner scanner = new Scanner(in);
             scanner.useDelimiter("\\A");
 
             boolean hasInput = scanner.hasNext();
-            if(hasInput){
+            if (hasInput) {
                 return scanner.next();
-            }else {
+            } else {
                 return null;
             }
         } finally {
